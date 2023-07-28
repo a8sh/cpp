cd26620635f85d60c5e924021d2b9e6dbdba89a8
notyou <101701422+a8sh@users.noreply.github.com>
2023-07-28 13:46:29 +0530

    rotation

---
+++ strings/stringRotation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isRotation(string& s1, string& s2){
+    if(s1.length()!=s2.length()) return false;
+    return ((s1+s2).find(s2)!=string::npos);
+}
+int main()
+{
+    string s1 = "citi";
+    string s2 = "tici";
+    if(isRotation(s1,s2)){
+        cout<<"True";
+    }
+    else{
+        cout<<"False";
+    }
+ return 0;
+}
